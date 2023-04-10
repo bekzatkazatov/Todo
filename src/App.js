5e503d7546dd04a3be0336775923e6d98b2152ad
bekzatkazatov <bekzatx2x@gmail.com>
2023-04-10 23:44:31 +0600

    todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import EditIcon from "./components/edit-icon"
+import DeleteIcon from "./components/delete-icon"
 
-function App() {
+
+ const App = () => {
+    const [todolist, setTodolist] = useState(list)
+    const [todo, setTodo] = useState('')
+
+    const handleAddTodo = () => {
+        if (todo.length) {
+            const newTodo = {
+                id: todolist.length + 1,
+                text: todo,
+                completed: false
+            }
+            setTodo('')
+            setTodolist([...todolist, newTodo])
+        }
+    }
+    const handleDelete = (id) => {
+        setTodolist(todolist.filter(todo => todo.id !== id))
+        }
+        const handleComplete =(id,e) =>{
+        setTodolist(todolist.map(todo=>todo.id === id ?{...todo,completed: e.target.checked}: todo))
+        }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={'wrapper'}>
+          <div>
+              <div className={'input-wrapper'}>
+                  <input
+                    value={todo}
+                    onChange={(e) => setTodo(e.target.value)}
+                      type="text"
+                  />
+                    <button className={'add-btn'} onClick={handleAddTodo}>
+                        add
+                    </button>
+              </div>
+              {
+                  todolist.map((todo) => (
+                      <div key={todo.id} className={`todo-wrapper ${todo.completed ?'completed': ''}`}>
+                          <span>{todo.text}</span>
+                          <div style={{display:"flex",alignItems:"center"}}>
+
+                              { !todo.completed &&
+                                  <button className={'edit-btn'}>
+                                      <EditIcon/>
+                                  </button>
+                              }
+                              <input type="checkbox"
+                                     onChange={(e)=>handleComplete(todo.id,e)}/>
+
+                              <button className={'edit-btn'}
+                              onClick={()=>handleDelete(todo.id)}>
+                                  <DeleteIcon/>
+                              </button>
+
+                          </div>
+                      </div>
+                  ))
+              }
+          </div>
+
+      </div>
   );
 }
 
 export default App;
+
+const list =[
+    {id:1,text:'Learn React',completed: false},
+    {id:2,text:'Learn Firebase',completed: false},
+    {id:3,text: 'Learn GraphQl',completed: false},
+    {id:4,text:'Learn React Native',completed: false}
+]
