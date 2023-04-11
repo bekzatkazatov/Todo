9e3e60fdd3ab8e614bf55b7152452f0a73270b91
bekzatkazatov <bekzatx2x@gmail.com>
2023-04-11 23:06:05 +0600

    tr

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
 import React, {useEffect, useState} from "react";
 import EditIcon from "./components/edit-icon"
 import DeleteIcon from "./components/delete-icon"
+import { nanoid } from 'nanoid'
+import axios from "axios";
 
 
  const App = () => {
-    const [todolist, setTodolist] = useState(list)
+    const [todolist, setTodolist] = useState([])
     const [todo, setTodo] = useState('')
      const [edit,setEdit] =useState(null)
 
-
+ useEffect(() => {
+        axios.get('https://64357ec2537112453fd7f9f7.mockapi.io/todos')
+         .then(res => setTodolist(res.data))
+     },[])
     const handleAddTodo = () => {
         if (todo.length) {
            if(!!edit){
-             setTodolist(todolist.map(item => item.id===edit ? {...item,text:todo}:item))
+
                setEdit(null)
                setTodo('')
+               axios.put(`https://64357ec2537112453fd7f9f7.mockapi.io/todos/${edit}`,{text:todo})
+                   .then (({data}) =>{
+                        setTodolist(todolist.map(item => item.id===data.id ? data:item))
+                   })
            }else {
                const newTodo = {
-                   id: todolist.length + 1,
                    text: todo,
                    completed: false
                }
+               console.log(newTodo ,'newTodo')
                setTodo('')
-               setTodolist([...todolist, newTodo])
+
+               axios.post ('https://64357ec2537112453fd7f9f7.mockapi.io/todos',newTodo)
+                   .then (res => {
+                       console.log(res.data,'res.data')
+                       setTodolist([...todolist,res.data])
+                   })
            }
         }
     }
-
+     console.log(todolist)
 
     const handleDelete = (id) => {
         setTodolist(todolist.filter(todo => todo.id !== id))
@@ -38,44 +52,56 @@ import DeleteIcon from "./components/delete-icon"
         const handleComplete =(id,e) =>{
         setTodolist(todolist.map(todo=>todo.id === id ?{...todo,completed: e.target.checked}: todo))
         }
+        const handleCompleteAll =(event) =>{
+        setTodolist(todolist.map(todo =>({...todo,completed: event.target.checked})))
+        }
 
   return (
       <div className={'wrapper'}>
           <div>
-              <div className={'input-wrapper'}>
-                  <input
-                    value={todo}
-                    onChange={(e) => setTodo(e.target.value)}
-                      type="text"
-                  />
-                    <button className={'add-btn'} onClick={handleAddTodo}>
-                        { !edit ? 'add' : 'edit'
+              <div style={{display:"flex",alignItems:"center",marginBottom:"10px",gap:'10px'}}>
+                  <input type="checkbox" onChange={handleCompleteAll} disabled={!!edit}/>
+                  <div className={'input-wrapper'}>
+                      <input
+                          value={todo}
+                          onChange={(e) => setTodo(e.target.value)}
 
-                        }
+                          type="text"
+                      />
+                      <button className={'add-btn'} onClick={handleAddTodo}>
+                          { !edit ? 'add' : 'edit'
 
-                    </button>
+                          }
+
+                      </button>
+                  </div>
               </div>
               {
                   todolist.map((todo) => (
-                      <div key={todo.id} className={`todo-wrapper ${todo.completed ?'completed': ''}`}>
-                          <span>{todo.text}</span>
-                          <div style={{display:"flex",alignItems:"center"}}>
+                      <div key={todo.id} style={{display:"flex",alignItems:"center",gap:'10px'}}>
+                          <input type="checkbox"
+                                 checked={todo.completed}
+                                 disabled={edit === todo.id}
+                                 onChange={(e)=>handleComplete(todo.id,e)}/>
+                          <div className={`todo-wrapper ${todo.completed ?'completed': ''}`}>
+                              <span>{todo.text}</span>
+                              <div style={{display:"flex",alignItems:"center"}}>
 
-                              { !todo.completed &&
-                                  <button
-                                      onClick={ (e) =>handleEdit(todo)}
-                                      className={'edit-btn'}>
-                                      <EditIcon/>
+                                  { !todo.completed &&
+                                      <button
+                                          onClick={ (e) =>handleEdit(todo)}
+                                          className={'edit-btn'}>
+                                          <EditIcon/>
+                                      </button>
+                                  }
+
+
+                                  <button className={'edit-btn'}
+                                          onClick={()=>handleDelete(todo.id)}>
+                                      <DeleteIcon/>
                                   </button>
-                              }
-                              <input type="checkbox"
-                                     onChange={(e)=>handleComplete(todo.id,e)}/>
 
-                              <button className={'edit-btn'}
-                              onClick={()=>handleDelete(todo.id)}>
-                                  <DeleteIcon/>
-                              </button>
-
+                              </div>
                           </div>
                       </div>
                   ))
@@ -88,9 +114,4 @@ import DeleteIcon from "./components/delete-icon"
 
 export default App;
 
-const list =[
-    {id:1,text:'Learn React',completed: false},
-    {id:2,text:'Learn Firebase',completed: false},
-    {id:3,text: 'Learn GraphQl',completed: false},
-    {id:4,text:'Learn React Native',completed: false}
-]
+
